aa1cbf59dfed38898c82d873735e7821aff6fb2b
Jenilson Costa <jenilson.costa@globo.com>
2020-05-19 23:53:32 -0300

    Finalizado recuperação de senha

--- index.php
+++ index.php
@@ -185,6 +185,88 @@ $app->post("/admin/users/:iduser", function ($iduser) {
 
 
 
+	//recuperar senha
+	$app->get("/admin/forgot", function() {
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot");	
+
+});
+
+$app->post("/admin/forgot", function(){
+
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");
+	exit;
+
+});
+
+$app->get("/admin/forgot/sent", function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-sent");	
+
+});
+
+
+
+$app->get("/admin/forgot/reset", function(){
+
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	  $page = new Hcode\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+
+	));
+
+});
+
+
+//Rota pra redefinir a senha no prazo de 1 hora como foi a nossa validação 
+
+$app->post("/admin/forgot/reset", function(){
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+
+	User::setFogotUsed($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT,[
+		"cost"=>10
+	]);
+
+	$user->setPassword($password);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+
+	]);
+
+	$page->setTpl("forgot-reset-success");
+
+});
+
+
 
 
 
